118831756c08dcf67712a407323a5a0f20f15189
인테그랄 <108013145+inte99ral@users.noreply.github.com>
2022-12-17 03:25:34 +0900

    PG_42627_디스크_컨트롤러

---
+++ Programmers/Level3/No42627/Main.java
@@ -0,0 +1,11 @@
+package Programmers.Level3.No42627;
+
+class Main {
+  private static final String str = "hello, world";
+
+  public static void main(String[] args){
+    System.out.println(str);
+
+    return;
+  }
+}
