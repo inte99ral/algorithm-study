7407bb42990703cbcfbe67e2bcfec03e9a4607b6
인테그랄 <108013145+inte99ral@users.noreply.github.com>
2022-12-17 00:17:39 +0900

    setting change

--- .setting/Main.java
+++ .setting/Main.java
@@ -6,13 +6,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 class Main {
-    private static BufferedReader br;
+  private static BufferedReader br;
 
-    public static void main(String[] args) throws IOException{
-        System.setIn(new FileInputStream("input.txt"));
-        br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println(br.readLine());
+  public static void main(String[] args) throws IOException{
+    System.setIn(new FileInputStream("input.txt"));
+    br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println(br.readLine());
 
-        return;
-    }
+    return;
+  }
 }
