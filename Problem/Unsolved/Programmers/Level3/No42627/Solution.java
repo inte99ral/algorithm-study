f018d47ee23cfe7d63256764b6b8266a1bdf0a38
인테그랄 <108013145+inte99ral@users.noreply.github.com>
2023-03-30 23:51:16 +0900

    PG_42627

--- Problem/Unsolved/Programmers/Level3/No42627/Solution.java
+++ Problem/Unsolved/Programmers/Level3/No42627/Solution.java
@@ -32,26 +32,60 @@ class Solution {
     for(int i = 0; i < tempList.size(); i++)
       jobs[i / 2][i % 2] = tempList.get(i);
 
-    solutionClass.solution(jobs);
+    System.out.println("[ANSWER]: " + solutionClass.solution(jobs));
+
     br.close();
     return;
   }
 
   public int solution(int[][] jobs) {
     int answer = 0;
-    int time = 0;
-    PriorityQueue<int[]> pq = new PriorityQueue<int[]>((x, y)->((x[1] - x[0]) - (y[1] - y[0])));
+    int currTime = 0;
+    PriorityQueue<int[]> jobQueue = new PriorityQueue<int[]>((x, y) -> (x[0] - y[0]));
+    PriorityQueue<int[]> waitingQueue = new PriorityQueue<int[]>((x, y) -> (x[1] - y[1]));
     
     for(int[] job : jobs) 
-      pq.offer(job);
+      jobQueue.offer(job);
+
+    System.out.println("루프 체크1");
     
-    while(pq.size() != 0) {
-      int[] tempArr = pq.poll();
-      int waitingTime = (time < tempArr[0])? 0 : (time - tempArr[0]);
-      answer += tempArr[1] + waitingTime;
-      time = (time > tempArr[0])? time + tempArr[1] : tempArr[0] + tempArr[1];
+    while(jobQueue.size() != 0 || waitingQueue.size() != 0) {
+      // 현 시간에 밀려있는 작업들을 대기열로 이동
+      while(jobQueue.size() != 0 && jobQueue.peek()[0] < currTime){
+        waitingQueue.offer(jobQueue.poll());
+      }
+
+      // 대기열이 없다면 가장 먼저 요청이 들어온 것을 처리
+      if(waitingQueue.size() == 0) {
+        int[] currJob = jobQueue.poll();
+        currTime = currJob[0] + currJob[1];
+
+        // 작업은 곧바로 시작되어 작업 시간 만큼만 기다렸다
+        answer += currJob[1];
+
+        System.out.println("대기 X: " + currJob[1]);
+        System.out.println("curr: " + currTime);
+        System.out.println("[0]: " + currJob[0]);
+        System.out.println("[1]: " + currJob[1]);
+      }
+      // 대기열이 있다면 그 중에서 제일 빨리 끝낼 수 있는 것을 우선시
+      else {
+        int[] currJob = waitingQueue.poll();
+        currTime += currJob[1];
+
+        // 작업은 최신 시간에서 요청된 시간을 뺀 만큼 기다렸다
+        answer += currTime - currJob[0];
+
+        System.out.println("대기 O: " + (currTime - currJob[0] + currJob[1]));
+        System.out.println("curr: " + currTime);
+        System.out.println("[0]: " + currJob[0]);
+        System.out.println("[1]: " + currJob[1]);
+      }
     }
-    
-    return answer / jobs.length;
+
+    // 평균
+    answer /= jobs.length;
+
+    return answer;
   }
 }
