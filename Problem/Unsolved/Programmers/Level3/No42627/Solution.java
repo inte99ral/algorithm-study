a48bac92be13a2a81f392bc419d667bd9431b143
인테그랄 <108013145+inte99ral@users.noreply.github.com>
2023-03-31 23:48:45 +0900

    PG_42627

--- Problem/Unsolved/Programmers/Level3/No42627/Solution.java
+++ Problem/Unsolved/Programmers/Level3/No42627/Solution.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.PriorityQueue;
 
 class Solution {
   private static BufferedReader br;
@@ -40,52 +39,27 @@ class Solution {
 
   public int solution(int[][] jobs) {
     int answer = 0;
-    int currTime = 0;
-    PriorityQueue<int[]> jobQueue = new PriorityQueue<int[]>((x, y) -> (x[0] - y[0]));
-    PriorityQueue<int[]> waitingQueue = new PriorityQueue<int[]>((x, y) -> (x[1] - y[1]));
+
     
-    for(int[] job : jobs) 
-      jobQueue.offer(job);
-
-    System.out.println("루프 체크1");
-    
-    while(jobQueue.size() != 0 || waitingQueue.size() != 0) {
-      // 현 시간에 밀려있는 작업들을 대기열로 이동
-      while(jobQueue.size() != 0 && jobQueue.peek()[0] < currTime){
-        waitingQueue.offer(jobQueue.poll());
-      }
-
-      // 대기열이 없다면 가장 먼저 요청이 들어온 것을 처리
-      if(waitingQueue.size() == 0) {
-        int[] currJob = jobQueue.poll();
-        currTime = currJob[0] + currJob[1];
-
-        // 작업은 곧바로 시작되어 작업 시간 만큼만 기다렸다
-        answer += currJob[1];
-
-        System.out.println("대기 X: " + currJob[1]);
-        System.out.println("curr: " + currTime);
-        System.out.println("[0]: " + currJob[0]);
-        System.out.println("[1]: " + currJob[1]);
-      }
-      // 대기열이 있다면 그 중에서 제일 빨리 끝낼 수 있는 것을 우선시
-      else {
-        int[] currJob = waitingQueue.poll();
-        currTime += currJob[1];
-
-        // 작업은 최신 시간에서 요청된 시간을 뺀 만큼 기다렸다
-        answer += currTime - currJob[0];
-
-        System.out.println("대기 O: " + (currTime - currJob[0] + currJob[1]));
-        System.out.println("curr: " + currTime);
-        System.out.println("[0]: " + currJob[0]);
-        System.out.println("[1]: " + currJob[1]);
-      }
-    }
-
-    // 평균
-    answer /= jobs.length;
 
     return answer;
+  }
+
+  public void comb(int[][] jobs) {
+    combRecur(jobs, new int[jobs.length][jobs[0].length], 0, 0);
+    return;
+  }
+
+  public void combRecur(int[][] jobs, int[][] selected, int index, int length) {
+    if(index == jobs.length) {
+      for(int[] iArr : selected) {
+        System.out.print(iArr[0] + "-");
+      }
+    }
+    return;
+  }
+
+  public void cal(int[][] jobs) {
+
   }
 }
