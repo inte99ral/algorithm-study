4ed66194c2ef2833c7348326c22431e0d79fa0cc
인테그랄 <108013145+inte99ral@users.noreply.github.com>
2023-04-05 02:51:52 +0900

    BJ 1592

--- .example/Baekjoon/Main.java
+++ .example/Baekjoon/Main.java
@@ -5,15 +5,15 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-class Main {
+public class Main {
+
   private static BufferedReader br;
 
-  public static void main(String[] args) throws IOException{
+  public static void main(String[] args) throws IOException {
     System.setIn(new FileInputStream("input.txt"));
     br = new BufferedReader(new InputStreamReader(System.in));
 
-    for(String line = br.readLine();line != null;line = br.readLine()) 
-      System.out.println(line);
+    for (String line = br.readLine(); line != null; line = br.readLine()) System.out.println(line);
 
     br.close();
     return;
--- Problem/Solved/Baekjoon/Bronze/Rank2/BJ_1592/Main.java
+++ Problem/Solved/Baekjoon/Bronze/Rank2/BJ_1592/Main.java
@@ -1,14 +1,10 @@
-package Problem.Unsolved.Baekjoon.Bronze.Rank2.No1592;
-
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.StringTokenizer;
 
-class Main {
+public class Main {
 
   private static BufferedReader br;
   private static StringTokenizer st;
@@ -19,31 +15,29 @@ class Main {
   public static void main(String[] args) throws IOException {
     System.setIn(new FileInputStream("input.txt"));
     br = new BufferedReader(new InputStreamReader(System.in));
-    st = new StringTokenizer("1 5 5");
+    st = new StringTokenizer(br.readLine());
 
     N = Integer.parseInt(st.nextToken());
     M = Integer.parseInt(st.nextToken());
     L = Integer.parseInt(st.nextToken());
 
-    solution(N, M, L);
+    System.out.println(solution(N, M, L));
 
     br.close();
     return;
   }
 
   public static int solution(int N, int M, int L) {
-    List<Integer> players = new LinkedList<Integer>();
+    int[] players = new int[N];
     int index = 0;
     int count = 0;
 
-    for (int i = 0; i < N; i++) players.add(0);
+    players[index] = 1;
 
-    while (players.get(index) < M - 1) {
-      players.set(index, players.get(index) + 1);
-      if (players.get(index) % 2 == 1) {
-        // index = (index + L)
-      } else {}
+    while (players[index] < M) {
+      index = (N + ((index - L + (2 * L * (players[index] % 2))) % N)) % N;
       count++;
+      players[index]++;
     }
 
     return count;
