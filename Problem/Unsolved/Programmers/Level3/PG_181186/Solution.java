cac49a969a25e76671961f149e138a1197041127
인테그랄 <108013145+inte99ral@users.noreply.github.com>
2023-05-09 18:09:14 +0900

    Update PG 181186

--- Problem/Unsolved/Programmers/Level3/PG_181186/Solution.java
+++ Problem/Unsolved/Programmers/Level3/PG_181186/Solution.java
@@ -1,6 +1,7 @@
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Stack;
 
 class Solution {
 
@@ -18,16 +19,18 @@ class Solution {
   }
 
   // -- Methods
-  private boolean[][] matrix;
   private int answer;
   private int height;
   private int width;
+  private boolean[][] matrix;
+  private Stack<Integer> debugStack;
 
   public int solution(int n) {
     answer = 0;
     height = 3;
     width = n;
     matrix = new boolean[height][width];
+    // debugStack = new Stack<Integer>();
 
     recur(0, 0);
 
@@ -41,7 +44,9 @@ class Solution {
     }
 
     if (y >= height) {
+      if (answer >= 1000000007) answer = 0;
       answer++;
+      // System.out.println(debugStack.toString());
       return;
     }
 
@@ -56,11 +61,14 @@ class Solution {
       matrix[y][x + 1] = true;
       matrix[y][x + 2] = true;
 
+      // debugStack.push(1);
+
       recur(y, x + 3);
 
       matrix[y][x] = false;
       matrix[y][x + 1] = false;
       matrix[y][x + 2] = false;
+      // debugStack.pop();
     }
 
     // |
@@ -69,37 +77,46 @@ class Solution {
       matrix[y + 1][x] = true;
       matrix[y + 2][x] = true;
 
+      // debugStack.push(2);
+
       recur(y, x + 1);
 
       matrix[y][x] = false;
       matrix[y + 1][x] = false;
       matrix[y + 2][x] = false;
+      // debugStack.pop();
     }
 
     // ⌋
-    if (y - 1 >= 0 && x - 1 >= 0 && !matrix[y][x - 1] && !matrix[y - 1][x]) {
+    if (y + 1 < height && x - 1 >= 0 && !matrix[y + 1][x] && !matrix[y + 1][x - 1]) {
       matrix[y][x] = true;
-      matrix[y][x - 1] = true;
-      matrix[y - 1][x] = true;
+      matrix[y + 1][x] = true;
+      matrix[y + 1][x - 1] = true;
+
+      // debugStack.push(3);
 
       recur(y, x + 1);
 
       matrix[y][x] = false;
-      matrix[y][x - 1] = false;
-      matrix[y - 1][x] = false;
+      matrix[y + 1][x] = false;
+      matrix[y + 1][x - 1] = false;
+      // debugStack.pop();
     }
 
     // ⌊
-    if (y - 1 >= 0 && x + 1 < width && !matrix[y][x + 1] && !matrix[y - 1][x]) {
+    if (y + 1 < height && x + 1 < width && !matrix[y + 1][x] && !matrix[y + 1][x + 1]) {
       matrix[y][x] = true;
-      matrix[y][x + 1] = true;
-      matrix[y - 1][x] = true;
+      matrix[y + 1][x] = true;
+      matrix[y + 1][x + 1] = true;
 
-      recur(y, x + 2);
+      // debugStack.push(4);
+
+      recur(y, x + 1);
 
       matrix[y][x] = false;
-      matrix[y][x + 1] = false;
-      matrix[y - 1][x] = false;
+      matrix[y + 1][x] = false;
+      matrix[y + 1][x + 1] = false;
+      // debugStack.pop();
     }
 
     // ⌈
@@ -108,24 +125,30 @@ class Solution {
       matrix[y][x + 1] = true;
       matrix[y + 1][x] = true;
 
+      // debugStack.push(5);
+
       recur(y, x + 2);
 
       matrix[y][x] = false;
       matrix[y][x + 1] = false;
       matrix[y + 1][x] = false;
+      // debugStack.pop();
     }
 
     // ⌉
-    if (y + 1 < height && x - 1 >= 0 && !matrix[y][x - 1] && !matrix[y + 1][x]) {
+    if (y + 1 < height && x + 1 < width && !matrix[y][x + 1] && !matrix[y + 1][x + 1]) {
       matrix[y][x] = true;
-      matrix[y][x - 1] = true;
-      matrix[y + 1][x] = true;
+      matrix[y][x + 1] = true;
+      matrix[y + 1][x + 1] = true;
 
-      recur(y, x + 1);
+      // debugStack.push(6);
+
+      recur(y, x + 2);
 
       matrix[y][x] = false;
-      matrix[y][x - 1] = false;
-      matrix[y + 1][x] = false;
+      matrix[y][x + 1] = false;
+      matrix[y + 1][x + 1] = false;
+      // debugStack.pop();
     }
 
     return;
