d62dc41282e5ed1bc8d10e998dc058d1cbacef29
인테그랄 <108013145+inte99ral@users.noreply.github.com>
2023-07-18 15:57:16 +0900

    Solve PG 2941

--- Problem/Solved/Baekjoon/Silver/Ⅴ/BJ_2563_색종이/Main.java
+++ Problem/Solved/Baekjoon/Silver/Ⅴ/BJ_2563_색종이/Main.java
@@ -8,7 +8,7 @@ import java.io.InputStreamReader;
 public class Main {
 
   public static void main(String[] args) throws IOException, NumberFormatException {
-    System.setIn(new FileInputStream("input.txt"));
+    // System.setIn(new FileInputStream("input.txt"));
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int N = Integer.parseInt(br.readLine());
     boolean[][] matrix = new boolean[100][100];
