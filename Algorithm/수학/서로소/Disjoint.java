b48e7c929a5f9b01eb46abefe248b727c852efa6
인테그랄 <108013145+inte99ral@users.noreply.github.com>
2023-07-21 16:35:34 +0900

    update Algorithm

---
+++ Algorithm/수학/서로소/Disjoint.java
@@ -0,0 +1,51 @@
+import java.io.FileInputStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Disjoint {
+	private static int T;
+	private static int[] data;
+	
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("input.txt"));
+		Scanner sc = new Scanner(System.in);
+		T = sc.nextInt();
+		
+		for(int t = 1; t <= T; t++) {
+			int ans = 0;
+			
+			int N = sc.nextInt(),
+			int M = sc.nextInt();
+			
+			data = new int[N + 1];
+			
+			// makeSet
+			for(int i = 1; i <= N; i++) {
+				data[i] = i;
+			}
+			
+			for(int i = 0; i < M; i++) {
+				unionSet(sc.nextInt(), sc.nextInt());
+			}
+			
+			for(int i = 1; i <= N; i++) {
+				if(data[i] == i) ans ++;
+			}
+			
+			System.out.println("#" + t + " " + ans);
+		}
+	}
+	
+	public static void unionSet(int x, int y) {
+		int px = findSet(x);
+		int py = findSet(y);
+		data[py] = px;
+	}
+
+	public static int findSet(int x) {
+		if(x == data[x]) { 
+			return x;
+		}
+		return data[x] = findSet(data[x]);
+	}
+}
