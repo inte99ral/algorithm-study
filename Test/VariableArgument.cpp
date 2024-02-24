085b9ec652ca93b8a0e2a76bd8953749d824d065
인테그랄 <inte99ral@gmail.com>
2024-02-24 15:24:47 +0900

    Update Test/VariableArgument

--- Test/VariableArgument.cpp
+++ Test/VariableArgument.cpp
@@ -3,13 +3,14 @@
  */
 #include <cstdarg>
 #include <iostream>
+#include <typeinfo>
 
 using namespace std;
 
 void func(int args, ...) {
   va_list list;
 
-  va_start(list, args);
+  va_start(list, args);  // 마지막 고정인수
 
   for (int i = 0; i < args; i++) {
     int temp = va_arg(list, int);
@@ -21,6 +22,14 @@ void func(int args, ...) {
 
 int main() {
   func(4, 'c', 'g', 'x', '\0');
+
+  int arr1[2][3] = {{1, 2, 3}, {4, 5, 6}};
+  int arr2[3] = {1, 2, 3};
+  int arr3 = 1;
+
+  cout << typeid(arr1).name() << endl;
+  cout << typeid(arr2).name() << endl;
+  cout << typeid(arr3).name() << endl;
 
   return 0;
 }
