f0eb77490b7909b2dc095e84b2540a80e52534d3
인테그랄 <inte99ral@gmail.com>
2024-03-05 01:47:25 +0900

    Update Test/test.hpp

--- Documentation/Common/C++/Test.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef TEST_HPP
-#define TEST_HPP
-
-int func1();
-
-#endif
---
+++ Test/Tes.cpp
@@ -0,0 +1,7 @@
+// #include ".\Documentation\Common\C++\Test.hpp"
+
+using namespace std;
+
+int main() {
+  return 0;
+}
