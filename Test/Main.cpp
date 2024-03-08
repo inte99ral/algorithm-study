66c42b1276a1aecc36df9c16f02c216b4f8edae7
인테그랄 <inte99ral@gmail.com>
2024-03-08 23:25:26 +0900

    Update Test

--- Test/Main.cpp
+++ Test/Main.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 #include <regex>
+#include <typeinfo>
 #include <vector>
+
+#define VERSION 3.1f
 
 using namespace std;
 
 int main() {
-  char cArr[] = "db-123-log.txt";
+  cout << "[VERSION]: " << VERSION << '\n';
 
-  regex regExp1("[0-9]+");
+  char cArr[] = "db-123-456-789-log.txt";
+
+  regex regExp1("[0-9]+", regex::optimize);
   regex regExp2("db-\\d*-log\\.txt");
 
   cout << (string)cArr << " : " << sizeof(cArr) / sizeof(char) << " : " << boolalpha << regex_match(&cArr[0], &cArr[14], regExp2) << '\n';
+
+  cregex_token_iterator iter(cArr, cArr + 15, regExp1), end;
+
+  while (iter != end) {
+    cout << stoi(*iter++) << '\n'
+  }
 
   return 0;
 }
