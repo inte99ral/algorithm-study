14cce0929cbb7eb5e7026cff627394a3a496850c
인테그랄 <inte99ral@gmail.com>
2024-03-16 01:09:34 +0900

    Update Doc/Language_Reference

--- Test/Algorithm/Main.cpp
+++ Test/Algorithm/Main.cpp
@@ -1,5 +1,5 @@
 /**
- * Combination
+ * 순열과 조합 테스트
  */
 #include <deque>
 #include <iostream>
@@ -9,34 +9,21 @@
 ArrayTool at;
 
 using namespace std;
-// Prototype Declaration ====================
-int factorial(int x);
 
-// Implements Definition ====================
 int main() {
-  ios_base::sync_with_stdio(false);
+  // ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.tie(nullptr);
 
   const int N = 5;
   const int R = 2;
 
-  int origin[N][2] = {{1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}};
+  int origin[N][2] = {{1, 6}, {2, 7}, {3, 8}, {4, 9}, {5, 0}};
   int fixed[R][2] = {};
 
-  // Just Count ======================================
+  // Input ====================================
   {
-    // nPr = n! / (n - r)!
-    cout << "\n[CASES]: Don't know!\n";
-    cout << "[ANSWER]: " << factorial(N) / (factorial(N - R) * factorial(R)) << '\n';
-  }
-
-  // No Recursive ====================================
-  {
-    int count = 0;
     deque<pair<int, int>> task;  // select, length
-
-    cout << "\n[CASES]:\n";
 
     task.push_back({0, 0});
     while (task.size() != 0) {
@@ -45,7 +32,6 @@ int main() {
       task.pop_back();
 
       if (length >= R) {
-        count++;
         at.print(fixed);
         continue;
       }
@@ -59,15 +45,9 @@ int main() {
       task.push_back({select + 1, length + 1});
     }
 
-    cout << "[ANSWER]: " << count << '\n';
+    at.print(origin);
+    at.print(fixed);
+
+    return 0;
   }
-  return 0;
-}
-
-int factorial(int x) { return (x == 1) || (x == 0) ? 1 : x * factorial(x - 1); }
-
-int combRecur() {
-}
-
-int comb() {
 }
---
+++ Test/Algorithm/Test.cpp
@@ -0,0 +1,21 @@
+#include "Test.hpp"
+
+// Implements Definition ====================
+// ArrayTool ======================
+void ArrayTool::printRecur(int* arr, std::deque<int> arrData) {
+  if (arrData.size() == 0) std::cout << "\033[0m" << *arr << ", ";
+  else {
+    int N = arrData.front();
+    arrData.pop_front();
+    int gap = 1;
+    int color = 31 + (arrData.size() % 6);
+    for (int i : arrData) gap *= i;
+    std::cout << "\033[" << color << "m[";
+
+    for (int n = 0; n < N; n++) {
+      printRecur(arr, arrData);
+      arr += gap;
+    }
+    std::cout << "\033[" << color << "m\b\b]\033[0m, ";
+  }
+}
