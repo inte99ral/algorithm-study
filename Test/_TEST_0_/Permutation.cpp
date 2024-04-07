f51ee82d9c232f6584da82caa535f3aae40354a7
인테그랄 <inte99ral@gmail.com>
2024-04-07 18:19:02 +0900

    Update Doc

--- Test/_TEST_0_/Permutation.cpp
+++ Test/_TEST_0_/Permutation.cpp
@@ -4,11 +4,11 @@
 #include <regex>
 
 using namespace std;
+
 // # Prototype Declaration ====================
 int factorial(int x);
-template <typename T, size_t N>
-void print(T (&arr)[N]);
-void printRecur(int* arr, std::deque<int> arrData);
+template <typename T, size_t N> void printArr(T (&arr)[N]);
+void printArrRecur(int* arr, std::deque<int> arrData);
 
 // # Implements Definition ====================
 int main() {
@@ -16,12 +16,12 @@ int main() {
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  const int N = 5;
-  const int R = 2;
+  const int N = 4;
+  const int R = 3;
 
-  int origin[N][3] = {{1, 11, 111}, {2, 22, 222}, {3, 33, 333}, {4, 44, 444}, {5, 55, 555}};
+  int origin[N][3] = {{1, 11, 111}, {2, 22, 222}, {3, 33, 333}, {4, 44, 444}};
 
-  /* 1. Mathematical Calculation ============ *
+  /* 1. Mathematical Calculation ============ */
   {
     // nPr = n! / (n - r)!
     cout << "\n[CASES]: Don't know!\n";
@@ -29,34 +29,44 @@ int main() {
   }
   /* ================== †† ================== */
 
-
   /* 2. For Loop ============================ */
   {
-    // R 값 수정 불가 3개 뽑기
+    // 뽑는 수 R 값 수정 불가 3 고정. 뽑는 수 만큼 루프
     int count = 0;
-    int select[R][3] = {};
+    int select[3][3] = {};
 
-    for (int i = 0; i < N - 2; i++) {
+    cout << "\n[CASES]:\n";
+
+    for (int i = 0; i < N; i++) {
       copy((int*)origin[i], (int*)origin[i + 1], (int*)select[0]);
-      for (int j = 0; j < N - 1; j++) {
+      for (int j = 0; j < N; j++) {
+        if(j == i) continue;
+        copy((int*)origin[j], (int*)origin[j + 1], (int*)select[1]);
         for (int k = 0; k < N; k++) {
+          if(k == i || k == j) continue;
+          copy((int*)origin[k], (int*)origin[k + 1], (int*)select[2]);
+          printArr(select);
+          count++;
         }
       }
     }
 
-    cout << "\n[CASES]:\n";
+    cout << "[ANSWER]: " << count << '\n';
   }
   /* ================== †† ================== */
 
   /* 3. Stack =============================== */
   {
-
+    // 뽑는 수 R 값 수정 불가 3 고정. 뽑는 수 만큼 루프
+    int count = 0;
+    int select[R][3] = {};
   }
   /* ================== †† ================== */
+
   {  // ## 2. Stack ================================
-    // int count = 0;
-    // int select[R][3] = {};
-    // deque<array<int, 3>> task = {{0, 0, -1}};
+     // int count = 0;
+     // int select[R][3] = {};
+     // deque<array<int, 3>> task = {{0, 0, -1}};
 
     // cout << "\n[CASES]:\n";
 
@@ -86,18 +96,12 @@ int main() {
     // cout << "[ANSWER]: " << count << '\n';
   }
 
-  {
-    // ## 2. DFS With Stack =======================
-    // ## 3. Swap =======================
-  }
-
   return 0;
 }
 
 int factorial(int x) { return (x == 1) || (x == 0) ? 1 : x * factorial(x - 1); }
 
-template <typename T, size_t N>
-void print(T (&arr)[N]) {
+template <typename T, size_t N> void printArr(T (&arr)[N]) {
   std::deque<int> arrData;
   std::string arrType = typeid(arr).name();
   std::regex regExp("A[0-9]+_", std::regex::optimize);
@@ -109,11 +113,11 @@ void print(T (&arr)[N]) {
     arrData.push_back(std::stoi(token.substr(1, token.size() - 2)));
   }
 
-  printRecur((int*)arr, arrData);
+  printArrRecur((int*)arr, arrData);
   std::cout << "\b\b  " << std::endl;
 }
 
-void printRecur(int* arr, std::deque<int> arrData) {
+void printArrRecur(int* arr, std::deque<int> arrData) {
   if (arrData.size() == 0) std::cout << "\033[0m" << *arr << ", ";
   else {
     int N = arrData.front();
@@ -124,7 +128,7 @@ void printRecur(int* arr, std::deque<int> arrData) {
     std::cout << "\033[" << color << "m[";
 
     for (int n = 0; n < N; n++) {
-      printRecur(arr, arrData);
+      printArrRecur(arr, arrData);
       arr += gap;
     }
     std::cout << "\033[" << color << "m\b\b]\033[0m, ";
