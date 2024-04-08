a220f5f7a84e4e3d27f7c0095420c7db2646b6ad
인테그랄 <inte99ral@gmail.com>
2024-04-08 23:52:22 +0900

    Update Permutation.cpp

--- Test/_TEST_0_/Permutation.cpp
+++ Test/_TEST_0_/Permutation.cpp
@@ -1,4 +1,5 @@
 #include <array>
+#include <cstdarg>
 #include <deque>
 #include <iostream>
 #include <regex>
@@ -6,94 +7,57 @@
 using namespace std;
 
 // # Prototype Declaration ====================
+
 int factorial(int x);
+template <typename T, size_t N> 
+void printArr(T (&arr)[N], int begin = 0, ...);
+void printArrRecur(int* arr, deque<int> arrData);
+template <typename T, size_t N> 
+void swapArr(T (&arr1)[N], T (&arr2)[N]);
 template <typename T, size_t N>
-void printArr(T (&arr)[N]);
-void printArrRecur(int* arr, std::deque<int> arrData);
+void perm4(T (&origin)[N], int n, int r);
+template <typename T, size_t N>
+void perm4Recur(T (&select)[N], int size, int n, int r);
+
 
 // # Implements Definition ====================
+
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.tie(nullptr);
 
   const int N = 4;
-  const int R = 3;
+  const int R = 2;
 
   int origin[N][3] = {{1, 11, 111}, {2, 22, 222}, {3, 33, 333}, {4, 44, 444}};
 
-  /* 1. Mathematical Calculation ============ */
+  // ## 1. Mathematical Calculation =============
   {
     // nPr = n! / (n - r)!
-    cout << "\n[CASES]: Don't know!\n";
-    cout << "[ANSWER]: " << factorial(N) / factorial(N - R) << '\n';
+    // cout << "\n[CASES]: Don't know!\n";
+    // cout << "[ANSWER]: " << factorial(N) / factorial(N - R) << '\n';
   }
-  /* ================== †† ================== */
 
-  /* 2. For Loop ============================ */
+  // ## 2. Inline For Loop ======================
   {
-    // 뽑는 수 R 값 수정 불가 3 고정. 뽑는 수 만큼 루프
-    int count = 0;
-    int select[3][3] = {};
-
-    cout << "\n[CASES]:\n";
-
-    for (int i = 0; i < N; i++) {
-      copy((int*)origin[i], (int*)origin[i + 1], (int*)select[0]);
-      
-      for (int j = 0; j < N; j++) {
-        if (j == i) continue;
-        copy((int*)origin[j], (int*)origin[j + 1], (int*)select[1]);
-
-        for (int k = 0; k < N; k++) {
-          if (k == i || k == j) continue;
-          copy((int*)origin[k], (int*)origin[k + 1], (int*)select[2]);
-          printArr(select);
-          count++;
-        }
-      }
-    }
-
-    cout << "[ANSWER]: " << count << '\n';
-  }
-  /* ================== †† ================== */
-
-  /* 3. Stack =============================== */
-  {
-    int count = 0;
-    int select[R][3] = {};
-    deque<array<int, 3>> task = {{0, 0, -1}};
-
-    cout << "\n[CASES]:\n";
-  }
-  /* ================== †† ================== */
-
-  {  // ## 2. Stack ================================
-     // int count = 0;
-     // int select[R][3] = {};
-     // deque<array<int, 3>> task = {{0, 0, -1}};
+    // int count = 0;
+    // int select[3][3] = {};
 
     // cout << "\n[CASES]:\n";
 
-    // while (!task.empty()) {
-    //   int data = task.back()[0];
-    //   int size = task.back()[1];
-    //   int prev = task.back()[2];
-    //   task.pop_back();
+    // for (int i = 0; i < N; i++) {
+    //   copy((int*)origin[i], (int*)origin[i + 1], (int*)select[0]);
 
-    //   if (size != 0) {
-    //     copy((int*)origin[prev], (int*)origin[prev + 1], (int*)select[size - 1]);
-    //   }
+    //   for (int j = 0; j < N; j++) {
+    //     if (j == i) continue;
+    //     copy((int*)origin[j], (int*)origin[j + 1], (int*)select[1]);
 
-    //   if (size == R) {
-    //     print(select);
-    //     count++;
-    //     continue;
-    //   }
-
-    //   for (int n = N - 1; n >= 0; n--) {
-    //     if (!(data & (1 << n))) {
-    //       task.push_back({data | (1 << n), size + 1, n});
+    //     for (int k = 0; k < N; k++) {
+    //       if (k == i || k == j) continue;
+    //       copy((int*)origin[k], (int*)origin[k + 1], (int*)select[2]);
+    //       printArr(select);
+    //       count++;
     //     }
     //   }
     // }
@@ -101,42 +65,130 @@ int main() {
     // cout << "[ANSWER]: " << count << '\n';
   }
 
+  // ## 3. Inline Stack & Bitwise Operator ======
+  {
+    // // 선택한 데이터를 저장하기 위하여 비트 연산자를 사용하기 때문에 Int = 32bit, 32개 이상 선택 불가
+    // int count = 0;
+    // int select[R][3] = {};
+    // deque<array<int, 3>> task = {{0, 0, 0}};
+
+    // cout << "\n[CASES]:\n";
+
+    // while (!task.empty()) {
+    //   int data = task.back()[0];
+    //   int size = task.back()[1];
+    //   int curr = task.back()[2];
+
+    //   task.pop_back();
+
+    //   if (size == R) {
+    //     printArr(select);
+    //     count++;
+    //     continue;
+    //   }
+
+    //   if (curr == N) {
+    //     continue;
+    //   }
+
+    //   task.push_back({data, size, curr + 1});
+    //   if (data & (1 << curr)) continue;
+    //   copy((int*)origin[curr], (int*)origin[curr + 1], (int*)select[size]);
+    //   task.push_back({data | (1 << curr), size + 1, 0});
+    // }
+
+    // cout << "[ANSWER]: " << count << '\n';
+  }
+
+  // ## 4. Recursive Function & Swap ============
+  {
+    perm4(origin, N, R);
+  }
+
   return 0;
 }
 
-int factorial(int x) { return (x == 1) || (x == 0) ? 1 : x * factorial(x - 1); }
-
-template <typename T, size_t N>
-void printArr(T (&arr)[N]) {
-  std::deque<int> arrData;
-  std::string arrType = typeid(arr).name();
-  std::regex regExp("A[0-9]+_", std::regex::optimize);
-
-  std::sregex_token_iterator iter(arrType.begin(), arrType.end(), regExp), end;
-
-  while (iter != end) {
-    std::string token = (std::string)*iter++;
-    arrData.push_back(std::stoi(token.substr(1, token.size() - 2)));
-  }
-
-  printArrRecur((int*)arr, arrData);
-  std::cout << "\b\b  " << std::endl;
+int factorial(int x) { 
+  return (x == 1) || (x == 0) ? 1 : x * factorial(x - 1); 
 }
 
-void printArrRecur(int* arr, std::deque<int> arrData) {
-  if (arrData.size() == 0) std::cout << "\033[0m" << *arr << ", ";
+template <typename T, size_t N>
+void printArr(T (&arr)[N], int begin, ...) {
+  deque<int> arrData;
+  string arrType = typeid(arr).name();
+
+  regex regExp("A[0-9]+_", regex::optimize);
+  sregex_token_iterator iter(arrType.begin(), arrType.end(), regExp), end;
+
+  va_list ap;
+  va_start(ap, begin);
+  int buffer = begin;
+
+  while (iter != end) {
+    string token = (string)*iter++;
+    if (buffer) {
+      arrData.push_back(buffer);
+      buffer = va_arg(ap, int);
+    }
+    else {
+      arrData.push_back(stoi(token.substr(1, token.size() - 2)));
+    } 
+  }
+
+  va_end(ap);
+
+  printArrRecur((int*)arr, arrData);
+  cout << "\b\b  " << endl;
+}
+
+void printArrRecur(int* arr, deque<int> arrData) {
+  if (arrData.size() == 0) cout << "\033[0m" << *arr << ", ";
   else {
     int N = arrData.front();
     arrData.pop_front();
     int gap = 1;
     int color = 31 + (arrData.size() % 6);
     for (int i : arrData) gap *= i;
-    std::cout << "\033[" << color << "m[";
+    cout << "\033[" << color << "m[";
 
     for (int n = 0; n < N; n++) {
       printArrRecur(arr, arrData);
       arr += gap;
     }
-    std::cout << "\033[" << color << "m\b\b]\033[0m, ";
+    cout << "\033[" << color << "m\b\b]\033[0m, ";
   }
+}
+
+template <typename T, size_t N>
+void swapArr(T (&arr1)[N], T (&arr2)[N]) {
+  T temp[N] = {};
+  copy((int*)arr1, (int*)arr1 + size(arr1), (int*)temp);
+  copy((int*)arr2, (int*)arr2 + size(arr2), (int*)arr1);
+  copy((int*)temp, (int*)temp + size(temp), (int*)arr2);
+  return;
+}
+
+
+template <typename T, size_t N>
+void perm4(T (&origin)[N], int n, int r) {
+  T select[N] = {};
+  copy((int*)origin[0], (int*)origin[size(origin)], (int*)select);
+
+  perm4Recur(select, 0, n, r);
+  return;
+}
+
+template <typename T, size_t N>
+void perm4Recur(T (&select)[N], int size, int n, int r) {
+  if (size == r) {
+    printArr(select, r, NULL);
+    return;
+  }
+
+  for(int i = size; i < n; i++) {
+    swapArr(select[size], select[i]);
+    perm4Recur(select, size + 1, n, r);
+    swapArr(select[size], select[i]);
+  }
+  return;
 }
