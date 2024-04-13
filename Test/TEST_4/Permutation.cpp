6af19364cf83962d808cb3dc0c9947707e1291ca
인테그랄 <inte99ral@gmail.com>
2024-04-14 00:36:15 +0900

    Update Doc
    * 버그 픽스중

--- Test/TEST_4/Permutation.cpp
+++ Test/TEST_4/Permutation.cpp
@@ -15,6 +15,7 @@ void printArrRecur(int* arr, deque<int> arrData);
 
 // # Implements Definition ====================
 // ## 0. Default ===============================
+
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
@@ -26,6 +27,7 @@ int main() {
   int origin[N][3] = {{1, 11, 111}, {2, 22, 222}, {3, 33, 333}, {4, 44, 444}, {5, 55, 555}};
 
   // ## 3. Inline : While Loop ===================
+  goto section3end;
   {
     int select[R][3] = {};
     int count = 0;
@@ -73,6 +75,44 @@ int main() {
 
     cout << "[ANSWER]: " << count << '\n';
   }
+  section3end:
+
+  // ## 4. Inline Stack & Bitwise Operator =======
+  // goto section4end;
+
+  {
+    // 비트 연산자를 사용하기 때문에 Int = 32bit, 32개 이상 선택 불가. bool 배열로 대체 가능
+    int count = 0;
+    int select[R][3] = {};
+    deque<array<int, 3>> task = {{0, 0, 0}};
+
+    cout << "\n[CASES]:\n";
+
+    while (!task.empty()) {
+      int data = task.back()[0];
+      int size = task.back()[1];
+      int curr = task.back()[2];
+
+      task.pop_back();
+
+      if (size == R) {
+        printArr(select);
+        count++;
+        continue;
+      }
+
+      if (curr == N) {
+        continue;
+      }
+
+      task.push_back({data, size, curr + 1});
+      if (data & (1 << curr)) continue;
+      copy((int*)origin[curr], (int*)origin[curr + 1], (int*)select[size]);
+      task.push_back({data | (1 << curr), size + 1, 0});
+  }
+  section4end:
+
+
 
   return 0;
 }
