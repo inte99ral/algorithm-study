eb5af9dbc1261d0247aacc7b2a54f94d23bbd7b0
인테그랄 <inte99ral@gmail.com>
2024-04-15 19:00:14 +0900

    Move Common To Doc/L.R/ETC

--- Test/TEST_5/Permutation.cpp
+++ Test/TEST_5/Permutation.cpp
@@ -3,6 +3,7 @@
 #include <deque>
 #include <iostream>
 #include <regex>
+#include <algorithm>
 
 using namespace std;
 
@@ -28,9 +29,14 @@ int main() {
 
   // ## 5. Iterative =============================
   {
+    // https://www.google.co.kr/search?q=%EC%88%9C%EC%97%B4+swap+reverse+%EC%98%A4%EB%A6%84%EC%B0%A8%EC%88%9C&sca_esv=954f74c53a241618&sca_upv=1&hl=ko&sxsrf=ACQVn0-L2vr_gqJ4iiFrsXhFa4B33rAmGQ%3A1712642043933&ei=-9cUZv7SOP-Nvr0Py4etsAk&ved=0ahUKEwi-vJ_luLSFAxX_hq8BHctDC5YQ4dUDCBA&uact=5&oq=%EC%88%9C%EC%97%B4+swap+reverse+%EC%98%A4%EB%A6%84%EC%B0%A8%EC%88%9C&gs_lp=Egxnd3Mtd2l6LXNlcnAiIOyInOyXtCBzd2FwIHJldmVyc2Ug7Jik66aE7LCo7IicMggQABiJBRiiBDIIEAAYgAQYogQyCBAAGIAEGKIESJShiwFQoo6LAVi9oIsBcAR4AZABAJgBpAKgAbAJqgEFMC43LjG4AQPIAQD4AQGYAgigApcEwgIKEAAYRxjWBBiwA8ICBxAjGLACGCeYAwCIBgGQBgqSBwM0LjSgB9UU&sclient=gws-wiz-serp
+    // https://velog.io/@ddongh1122/%EC%88%9C%EC%97%B4-Permutation-%EA%B5%AC%ED%98%84
+    // https://zzingonglog.tistory.com/18
+    // https://mjmjmj98.tistory.com/38
     // cout << "\n[CASES]:\n";
     // int count = perm5(origin, N, R);
     // cout << "[ANSWER]: " << count << '\n';
+    next_permutation(origin, origin + 5);
   }
 
 
