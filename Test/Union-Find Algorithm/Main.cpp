23362872b134dde45ac55ab089c6aea223fe48bf
인테그랄 <inte99ral@gmail.com>
2024-05-26 23:45:14 +0900

    Update Doc

--- Test/Union-Find Algorithm/Main.cpp
+++ Test/Union-Find Algorithm/Main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
@@ -9,32 +10,59 @@ void unionSet(int x, int y);
 
 // Global Variable & Constant================
 const string input =
-  "10 21\n"
-  "0 1 9\n"
-  "0 2 9\n"
-  "0 9 8\n"
-  "0 8 18\n"
-  "1 2 3\n"
-  "1 4 6\n"
-  "2 3 2\n"
-  "2 4 4\n"
-  "2 9 9\n"
-  "3 4 2\n"
-  "3 5 9\n"
-  "3 9 8\n"
-  "4 5 9\n"
-  "5 6 4\n"
-  "5 7 5\n"
-  "5 9 7\n"
-  "6 7 1\n"
-  "6 8 4\n"
-  "7 8 3\n"
-  "7 9 9\n"
-  "8 9 10\n"
-  "8 10 18\n"
-  "9 10 8\n";
+  "9 8\n"
+  "1 2\n"
+  "1 4\n"
+  "2 4\n"
+  "3 5\n"
+  "5 7\n"
+  "6 8\n"
+  "6 9\n"
+  "8 9\n";
+
+int *nodes;
+int **edges;
   
 // Implements Definition ====================
 int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int V;
+  int E;
+
+  {
+    stringbuf inputbuf(input);
+    streambuf *backup = cin.rdbuf(&inputbuf);
+
+    cin >> V >> E;
+
+    nodes = new int[V];
+    edges = new int*[E];
+
+    for (int e = 0; e < E; e++) {
+      cin >> edges[e][0] >> edges[e][1] >> edges[e][2];
+    }
+
+    cin.clear();
+    cin.rdbuf(backup);
+  }
+
+  
+
   return 0;
+}
+
+void makeSet(int x) {
+  nodes[x] = x;
+}
+
+int findSet(int x) {
+  if (nodes[x] == x) return x;
+  return nodes[x] = findSet(nodes[x]); // Path Compression
+}
+
+void unionSet(int x, int y) {
+  nodes[findSet(y)] = findSet(x);
 }
