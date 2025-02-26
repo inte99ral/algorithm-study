c1b41224805af66f2121a9f63314f2b076c47e0a
integral <inte99ral@gmail.com>
2025-02-26 10:10:28 +0900

    try: BJ 5397

--- Problem/Failed/Baekjoon/Silver/2/BJ_5397/_OLD_/Main.cpp
+++ Problem/Failed/Baekjoon/Silver/2/BJ_5397/_OLD_/Main.cpp
@@ -1,48 +1,52 @@
 #include <bits/stdc++.h>
 
+#ifndef ONLINE_JUDGE
+#define SET_IO(INPUT_DATA) std::ios::sync_with_stdio(false);std::cin.tie(nullptr);std::cout.tie(nullptr);std::ifstream IO_FS(INPUT_DATA);std::streambuf *IO_BACKUP=std::cin.rdbuf(IO_FS.is_open()?((std::istream*)&IO_FS)->rdbuf():((std::istream*)new std::stringstream(INPUT_DATA))->rdbuf())
+#define TEST_IO(TEST_CODE) TEST_CODE
+#define UNSET_IO() std::cin.rdbuf(IO_BACKUP)
+#else
+#define SET_IO(INPUT_PATH) std::ios::sync_with_stdio(false);std::cin.tie(nullptr);std::cout.tie(nullptr)
+#define TEST_IO(TEST_CODE) ((void) 0)
+#define UNSET_IO() ((void) 0)
+#endif
+
+
 using namespace std;
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-  cout.tie(nullptr);
-
-  #ifndef ONLINE_JUDGE
-  freopen("Problem\\Failed\\Baekjoon\\Silver\\2\\BJ_5397\\question\\input.txt", "rt", stdin);
-  #endif
+  SET_IO("_INPUT_.txt");
 
   int T;
-  string str;
-  list<char> cList;
-  list<char>::iterator iter;
-  
   cin >> T;
+  cin.ignore();
 
-  for (int t = 0; t < T; t++) {
-    cin >> str;
-    cList = list<char>();
-    iter = cList.begin();
+  while(T--) {
+    string keylog;
+    list<char> pw;
+    list<char>::iterator iter = pw.begin();
 
-    for (char c : str) {
-      switch (c) {
-        case '-':
-          if (iter != cList.begin()) iter = cList.erase(--iter);
-          break;
-        case '<':
-          if (iter != cList.begin()) iter--;
-          break;
-        case '>':
-          if (iter != cList.end()) iter++;
-          break;
-        default:
-          cList.insert(iter, c);
-          break;
+    cin >> keylog;
+
+    for (char key : keylog) {
+      switch (key) {
+       case '<':
+        if (iter != pw.begin()) iter--;
+        break;
+       case '>':
+        if (iter != pw.end()) iter++;
+        break;
+       case '-':
+        if (iter != pw.begin()) iter = pw.erase(--iter);
+        break;
+       default:
+        pw.insert(iter, key);
+        break;
       }
     }
 
-    for (char c : cList) cout << c;
-    cout << '\n';
+    cout << string(pw.begin(), pw.end()) << "\n";
   }
 
-  return 0;  
+  UNSET_IO();
+  return 0;
 }
