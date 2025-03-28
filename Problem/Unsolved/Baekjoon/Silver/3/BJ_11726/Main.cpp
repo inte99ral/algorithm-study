f89c985ec299d0addad6e20fde995f0d686677a1
integral <inte99ral@gmail.com>
2025-03-28 23:59:11 +0900

    try: BJ 11726

--- Problem/Unsolved/Baekjoon/Silver/3/BJ_11726/Main.cpp
+++ Problem/Unsolved/Baekjoon/Silver/3/BJ_11726/Main.cpp
@@ -25,11 +25,35 @@
 
 using namespace std;
 
+vector<int> factorial_memory(1001, 0);
+
+int factorial(int num) {
+  if (num <= 1) return 1;
+  if (!factorial_memory[num]) factorial_memory[num] = num * factorial(num - 1);
+  return factorial_memory[num];
+}
+
 int main(int argc, char* argv[]) {
   SET_IO("_INPUT_.txt");
 
-  for (string buf; cin >> buf;) {
-    cout << buf << endl;
+  int N;
+  int answer;
+  
+  cin >> N;
+  answer = 2;
+
+  { // * MEMO 00
+    /**
+     * N 이다 라고 했을때, x C y 계산을 어떻게 해야하는가?
+     * N 이 짝수이면 y 가 카운터 i 가 될꺼고 i 은 최대 N 이었을것, 또한 2씩 올라가야한다.
+     */
+  }
+
+  { // * TASK 00: 루프 연산
+
+    for (int i = (N % 2) + 2; i < N; i += 2) {
+      cout << "| = " << i << ", || = " << (N - i) / 2 << "\n";
+    }
   }
 
   UNSET_IO();
