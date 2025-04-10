2927da0c3f0291788c2ff58e877702ccbc66cf25
integral <inte99ral@gmail.com>
2025-04-10 16:33:54 +0900

    try: SWEA 20671

--- .example/SWEA/Main.cpp
+++ .example/SWEA/Main.cpp
@@ -1,23 +1,75 @@
+#define LOCAL_JUDGE 0 // <-- 로컬파일 입력용
+
+// # 『DIRECTORY_PATH』
+
+// ## Documentation ==========================================================
 /**
- * @link https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=5&problemLevel=6&contestProbId=AYxCewMqiqwDFASu&categoryId=AYxCewMqiqwDFASu&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=6&pageSize=10&pageIndex=1
  * @name 19118. 언덕길
- * @version D5
+ * @see "https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=5&problemLevel=6&contestProbId=AYxCewMqiqwDFASu&categoryId=AYxCewMqiqwDFASu&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=6&pageSize=10&pageIndex=1"
+ * @author inte99ral
+ * @version 2024-11-18
  */
 
-#include<iostream>
+
+// ## Import Declaration =====================================================
+
+// ### API & Library:
+
+#include <bits/stdc++.h>
+// #include <iostream>
+// #include <fstream>
+// #include <sstream>
+// #include <filesystem>
+
+#ifdef LOCAL_JUDGE
+  #define SET_IO(INPUT_DATA) \
+    std::ios::sync_with_stdio(false); \
+    std::cin.tie(nullptr); \
+    std::cout.tie(nullptr); \
+    std::istream* IO_S = (std::filesystem::exists(INPUT_DATA)) \
+      ? (std::istream*) new std::ifstream(INPUT_DATA) \
+      : (std::istream*) new std::stringstream(INPUT_DATA); \
+    std::streambuf* IO_BACKUP = std::cin.rdbuf(IO_S->rdbuf())
+
+  #define UNSET_IO() \
+    std::cin.rdbuf(IO_BACKUP); \
+    delete IO_S
+    
+#else
+  #define SET_IO(INPUT_DATA) \
+    std::ios::sync_with_stdio(false); \
+    std::cin.tie(nullptr); \
+    std::cout.tie(nullptr)
+
+  #define UNSET_IO() ((void) 0)
+#endif
+
+// ### Namespace:
 
 using namespace std;
-// # Prototype Declaration ====================
 
-// # Global Variable & Constant================
-const int CODE_VERSION = 0;
+// ## Variable & Constant ====================================================
 
-// # Implements Definition ====================
-// ## Main
-int main(int argc, char** argv) {
+const long long MOD_NUM = 1000000007;
+
+// ## Prototype Declaration ==================================================
+
+// ## Implements Definition ==================================================
+
+// ### Main
+
+int main() {
+
+	// SET_IO(
+  //   "INNER1 \n"
+  //   "INNER2 \n"
+  //   "INNER3 \n"
+  // );
+
+	SET_IO("_INPUT_.txt");
+
 	int T;
-
-	freopen("Problem/Unsolved/SWEA/D5/SWEA_19118/input.txt", "rt", stdin);
+	int N;
 
 	cin >> T;
 
@@ -28,5 +80,7 @@ int main(int argc, char** argv) {
 		 */
 		/////////////////////////////////////////////////////////////////////////////////////////////
 	}
+
+	UNSET_IO();
 	return 0;
 }
--- Problem/Unsolved/SWEA/D5/SWEA_20671/Main.cpp
+++ Problem/Unsolved/SWEA/D5/SWEA_20671/Main.cpp
@@ -1,21 +1,72 @@
-#include <iostream>
-#include <functional>
-#include <queue>
-#include <algorithm>
+#define LOCAL_JUDGE 0 // <-- 로컬파일 입력용
+
+#include <bits/stdc++.h>
+
+#ifdef LOCAL_JUDGE
+  #define SET_IO(INPUT_DATA) \
+    std::ios::sync_with_stdio(false); \
+    std::cin.tie(nullptr); \
+    std::cout.tie(nullptr); \
+    std::istream* IO_S = (std::filesystem::exists(INPUT_DATA)) \
+      ? (std::istream*) new std::ifstream(INPUT_DATA) \
+      : (std::istream*) new std::stringstream(INPUT_DATA); \
+    std::streambuf* IO_BACKUP = std::cin.rdbuf(IO_S->rdbuf())
+
+  #define UNSET_IO() \
+    std::cin.rdbuf(IO_BACKUP); \
+    delete IO_S
+    
+#else
+  #define SET_IO(INPUT_DATA) \
+    std::ios::sync_with_stdio(false); \
+    std::cin.tie(nullptr); \
+    std::cout.tie(nullptr)
+
+  #define UNSET_IO() ((void) 0)
+#endif
+
+// ### Namespace:
 
 using namespace std;
 
+// ## Variable & Constant ====================================================
+
 const long long MOD_NUM = 1000000007;
 
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
+// ## Prototype Declaration ==================================================
 
-	freopen("Problem\\Unsolved\\SWEA\\D5\\SWEA_20671\\question\\input.txt", "rt", stdin);  // * 로컬 input.txt 입력 코드
+// ## Implements Definition ==================================================
+
+// ### Main
+
+int main() {
+	SET_IO("_INPUT_.txt");
 
 	int T;
 	int N;
 
+	cin >> T;
+
+	for (int t = 1; t <= T; t++) {
+
+		cin >> N;
+
+		string *cardArr = new string[N * 2]();
+
+		for (int n = 0; n < N;) {
+			cin >> cardArr[n++];
+			cin >> cardArr[n++];
+		}
+
+		for (int n = 0; n < N;) {
+			cout << cardArr[n++] << " ";
+			cout << cardArr[n++] << "\n";
+		}
+
+		cout << "#" << t << " \n\n";
+		delete[] cardArr;
+	}
+
+	UNSET_IO();
 	return 0;
 }
